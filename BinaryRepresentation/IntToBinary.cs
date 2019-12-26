7c76d3dfc5f37b19ba66ea98f904dd9a2cebfb72
Sergey <avgrundem@gmail.com>
2019-12-27 02:08:38 +0600

    Добавьте файлы проекта.

---
+++ BinaryRepresentation/IntToBinary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BinaryRepresentation
+{
+    /// <summary>
+    /// Статический утилитарный класс для преобразования целого десятичного значения в двоичное
+    /// </summary>
+    public static class IntToBinary
+    {
+        /// <summary>
+        /// Разрядность целого числа
+        /// </summary>
+        private static int BitNumber = 32;
+        /// <summary>
+        /// Метод, который преобразует целое десятичное значение в двоичное строковое
+        /// </summary>
+        /// <param name="number">число, которое необходимо преобразовать</param>
+        /// <returns></returns>
+        public static string Transform(int number)
+        {
+            StringBuilder sb = new StringBuilder();
+            int mask = 1;
+
+            for (int i = 0; i < BitNumber; i++)
+            {
+                if ((number & (mask << i)) > 0)
+                {
+                    sb.Append("1");
+                }
+                else
+                {
+                    sb.Append("0");
+                }
+            }
+
+            return new string(sb.ToString().Reverse().ToArray());
+        }
+    }
+}
---
+++ BinaryRepresentation/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BinaryRepresentation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            int number;
+
+            if (Int32.TryParse(textBox1.Text.Replace(".", ","), out number))
+            {
+                textBox2.Text = IntToBinary.Transform(number).TrimStart('0');
+            }
+            else
+            {
+                MessageBox.Show("Значение должно быть целым числом");
+            }
+        }
+    }
+}
